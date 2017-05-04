c7b0d686e794bc4adb444f1064eed42dcb7381e1
Rafael Lima <rafaelccomp@gmail.com>
2017-05-04 08:26:08 -0300

    Lista duplamente encadeada genérica funcionando

--- Aula12-GDList/GDList.h
+++ Aula12-GDList/GDList.h
@@ -10,6 +10,7 @@ typedef struct{
   TNo *inicio;
 }GDList;
 GDList* GDList_create();
+int GDList_insere_o(GDList*, void*, unsigned int, int (*cmp)(void*, void*));
 int GDList_insere(GDList*, void*, unsigned int);
 void GDList_print(GDList*, void (*print)(void*));
 
--- Aula12-GDList/main.c
+++ Aula12-GDList/main.c
@@ -1,29 +1,60 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include "GDList.h"
+
+int comp_int(void* s1, void* s2){
+  int *p1 = (int*) s1,
+      *p2 = (int*) s2;
+    return (*p1 < *p2)? -1: (*p1 == *p2)? 0: 1;
+  /*
+  if(*p1 < *p2){
+    printf("%d eh menor que %d\n", *p1, *p2);
+    return -1;
+  }else{
+    if(*p1 == *p2){
+      printf("%d eh igual que %d\n", *p1, *p2);
+      return (0);
+    }else{
+      printf("%d eh maior que %d\n", *p1, *p2);
+      return (1);
+    }
+  }*/
+}
+
+int comp_int_inv(void* s1, void *s2){
+    return -1*comp_int(s1, s2);
+}
 
 void print_int (void* t){
   int *p;
   p = (int*) t;
-  printf("%d \n", *p);
+  printf("%d ", *p);
 }
 
-void print_float (void* t){
+void print_float (void* t) {
   float *p;
-  p = (float*) t;
+  p = (float *) t;
   printf("%2.2f \n", *p);
 }
 
+int getInt(void* t){
+  int *p;
+  p = (int*) t;
+  return (*p);
+}
+
 int main(){
-  //int v[] = {1, 2, 3, 4, 5};
-  float v[] = {2.4, 4.2, 5.4, 3.2, 1.1};
+  int v[] = {6, 5, 3, 2, 1};
+  //float v[] = {2.4, 4.2, 5.4, 3.2, 1.1};
+  //char v[4][128] = {"Rafael Lima", "Yolanda Ribeiro", "Arthur Modesto", "Pedro Alves"};
   int sizeV = 5, i;
-  void (*fprint)(void*) = print_float;
+  void (*fprint)(void*) = print_int;
 
   GDList *L1 = GDList_create();
   for(i=0; i < sizeV; i++){
-    GDList_insere(L1, &v[i], sizeof(v[i]));
+    GDList_insere_o(L1, &v[i], sizeof(v[i]), comp_int_inv);
+      GDList_print(L1, fprint);
   }
-  GDList_print(L1, fprint);
+  //GDList_print(L1, fprint);
   return (0);
 }
